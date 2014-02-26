cec92f9b36adc167d121998f7bf1b2c66a918b2d
Dennis Bartlett <dennis@comparemetrics.com>
2014-02-26 11:22:35 -0600

    Initial Push

---
+++ Ascend.js
@@ -0,0 +1,15 @@
+module.exports = (function(param) {
+
+	var func = function() {
+		/// your code
+
+		return {
+			animate: function() {
+				// do the animation
+				return this;
+			}
+		}
+	}
+
+	return func;
+})();
