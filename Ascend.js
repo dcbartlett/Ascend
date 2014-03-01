f53db1b4506a682728964e2fb1bfb286ffba7ede
Dennis Bartlett <bartlett.dc.1@gmail.com>
2014-03-01 12:27:54 -0600

    Removed Console log for arguments

--- Ascend.js
+++ Ascend.js
@@ -1,7 +1,6 @@
 var Ascend = (function() {
 
 	return func = function() {
-		console.log(arguments[0]);
 
 		if (!this.controller) {
 			this.controller = new Leap.Controller({ enableGestures: true });
